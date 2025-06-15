786d6aa0cd57e79f798dcbdcdd6ed1fb03d6735a
lucasrispoli <134862082+Lucasprofissa007@users.noreply.github.com>
2025-06-15 14:46:34 -0300

    Finalizacao do footer

--- livrodevpetropolis/src/components/Footer/index.jsx
+++ livrodevpetropolis/src/components/Footer/index.jsx
@@ -1,23 +1,29 @@
 import { Link } from "react-router-dom";
 import * as styles from "./Footer.module.css";
+import githubLogo from "../image/github-logo.png";
 
 export default function Footer() {
   return (
     <footer>
       <div className={styles.container}>
         <div className={styles.logo}>
-          <p>
-            Direitos autorais ©2025 Livraria Dev Petrópolis . Todos os direitos
-            reservados.
-          </p>
+         
+          <p>Direitos autorais ©2025 Livraria Dev Petrópolis</p>
+          <p>Desenvolvido por equipe Serratec</p>  
+            <p>Todos os direitos reservados.</p> 
         </div>
-
         <div className={styles.btnNewPost}>
-          <Link to={"/posts"}>
-            <button>Novo Post</button>
-          </Link>
-        </div>
-      </div>
+       
+         <h5>Política de Privacidade | Termos de Uso</h5> 
+         <h5>Email: suporte@devpetropolis.com</h5>
+         <h5>Dúvidas? Fale conosco!</h5>
+         
+         <Link to={"/posts"}>
+        </Link><a href="https://github.com/Junior411917/Sebo-Online-React" target="_blank" rel="">
+        <img src={githubLogo} alt="GitHub" />
+        </a>
+    </div>
+</div>
     </footer>
   );
 }
