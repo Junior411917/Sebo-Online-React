4a73333fbae9c024a15229b8b8e60dd24166e499
duarterailla <railladuarte@hotmail.com>
2025-06-15 22:49:23 -0300

    Atualizações da noite-dark

--- livrodevpetropolis/src/App.jsx
+++ livrodevpetropolis/src/App.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
-import { BrowserRouter } from 'react-router-dom'
-import AppRouter from './routes/AppRouter'
+// /src/App.jsx
+
+import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "./components/context";
+import AppRouter from "./routes/AppRouter";
+
+
+
 
 export default function App() {
   return (
-    <BrowserRouter>
-        <AppRouter/>
-    </BrowserRouter>
-  )
+    <ThemeProvider>
+      <BrowserRouter>
+        <AppRouter />
+      </BrowserRouter>
+    </ThemeProvider>
+  );
 }
--- livrodevpetropolis/src/components/Header/index.jsx
+++ livrodevpetropolis/src/components/Header/index.jsx
@@ -1,9 +1,18 @@
 import { Link } from "react-router-dom";
 import * as styles from "./Header.module.css";
+import { useTheme } from "../context";
 
 export default function HeaderMain() {
+  const { darkMode, setDarkMode } = useTheme();
   return (
     <header className={styles.header}>
+      <button
+        id="toggleMode"
+        className={styles.themeToggle}
+        onClick={() => setDarkMode(!darkMode)}
+      >
+        {darkMode ? "☀️" : "🌙"}
+      </button>
       <div className={styles.container}>
         <h1 className={styles.logo}>Livraria Dev Petrópolis</h1>
         <Link to="/" className={styles.voltarLogin}>
